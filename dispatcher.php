948f66f4a4b9b5c8c6e8f42c9d6b744c8ef4565b
Jérémie Litzler <webdev.jl@gmail.com>
2016-01-15 07:24:11 +0100

    Setup of the application

---
+++ dispatcher.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Custom error handler to catch all the error and process them.
+ */
+require_once 'errorHandler.php';
+/*
+ * Start the session
+ */
+session_start();
+
+$errorLogger = new Library\Core\ErrorManager();
+try {
+  $app = new $appClassName($errorLogger);
+  $logGuid = \Library\Utility\TimeLogger::StartLogInfo($app, "PageLoad => " . FrameworkConstants_BaseUrl . $app->httpRequest()->requestURI());
+  $output = $app->run();
+  \Library\Utility\TimeLogger::EndLog($app, $logGuid);
+  if($app->httpRequest()->IsPost()) {
+    echo $output;
+  } else {
+    exit($output);
+  }
+} catch (\Exception $exc) {
+  $errorLogger->LogError($exc);
+  die();
+}
