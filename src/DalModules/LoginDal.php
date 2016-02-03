7982827d564cbd96d95db86b52418b119f4636de
Jérémie Litzler <webdev.jl@gmail.com>
2016-02-03 21:56:35 +0100

    #2
    bug fix

--- src/DalModules/LoginDal.php
+++ src/DalModules/LoginDal.php
@@ -11,15 +11,16 @@ class LoginDal extends Dal\BaseManager {
      * 
      * @param ProjectManager $pm
      * @return array the selected row in the db
+     * @todo add the second parameter to the BaseManager method.
      */
-    public function selectOne($user, Dal\DbQueryFilters $dbFilters) {
-        $dbConfig = new \Dal\DbStatementConfig($user, \Dal\DbExecutionType::SELECT, $dbFilters);
-        $dbConfig->setLimitClause(1);
-        $dbConfig->BuildSelectQuery();
-        $this->addDbConfigItem($dbConfig);
-
-        return $this->BindParametersAndExecute(TRUE);
-    }
+//    public function selectOne($user, Dal\DbQueryFilters $dbFilters) {
+//        $dbConfig = new \Dal\DbStatementConfig($user, \Dal\DbExecutionType::SELECT, $dbFilters);
+//        $dbConfig->setLimitClause(1);
+//        $dbConfig->BuildSelectQuery();
+//        $this->addDbConfigItem($dbConfig);
+//
+//        return $this->BindParametersAndExecute(TRUE);
+//    }
 
     public function countById($item) {
         
