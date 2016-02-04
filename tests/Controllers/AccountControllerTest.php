393de6e26c2c57c1401fb643b35095dce39b389c
Jérémie Litzler <webdev.jl@gmail.com>
2016-02-04 06:37:59 +0100

    #4

--- tests/Controllers/AccountControllerTest.php
+++ tests/Controllers/AccountControllerTest.php
@@ -11,26 +11,23 @@ use Puzzlout\WebIde\Controllers\AccountController;
 
 class AccountControllerTest extends \PHPUnit_Framework_TestCase {
 
-  protected $app;
+    protected $app;
 
-  /**
-   * Initialize the app object.
-   */
-  protected function setUp()
-  {
-      $this->app = new \Puzzlout\WebIde\Tests\TestApplication();
-  }
-  
-  /**
-   * This method is generated.
-   */
-  public function testInstanceIsCorrect()
-  {
-    $this->assertNotNull($this->app);
-    $result = new AccountController($this->app,'Test','Test');
-    $this->assertInstanceOf('Puzzlout\WebIde\Controllers\AccountController', $result);
-  }
-  
-  //Write the next tests below...
-  
+    /**
+     * Initialize the app object.
+     */
+    protected function setUp() {
+        $this->app = new \Puzzlout\WebIde\Tests\TestApplication();
+    }
+
+    /**
+     * This method is generated.
+     */
+    public function testInstanceIsCorrect() {
+        $this->assertNotNull($this->app);
+        $result = new AccountController($this->app, 'Test', 'Test');
+        $this->assertInstanceOf('Puzzlout\WebIde\Controllers\AccountController', $result);
+    }
+
+    //Write the next tests below...
 }
--- tests/DalModules/LoginDalTest.php
+++ tests/DalModules/LoginDalTest.php
@@ -11,26 +11,23 @@ use Puzzlout\WebIde\DalModules\LoginDal;
 
 class LoginDalTest extends \PHPUnit_Framework_TestCase {
 
-  protected $app;
+    protected $app;
 
-  /**
-   * Initialize the app object.
-   */
-  protected function setUp()
-  {
-      $this->app = new \Puzzlout\WebIde\Tests\TestApplication();
-  }
-  
-  /**
-   * This method is generated.
-   */
-  public function testInstanceIsCorrect()
-  {
-    $this->assertNotNull($this->app);
-    $result = new LoginDal($this->app,NULL);
-    $this->assertInstanceOf('Puzzlout\WebIde\DalModules\LoginDal', $result);
-  }
-  
-  //Write the next tests below...
-  
+    /**
+     * Initialize the app object.
+     */
+    protected function setUp() {
+        $this->app = new \Puzzlout\WebIde\Tests\TestApplication();
+    }
+
+    /**
+     * This method is generated.
+     */
+    public function testInstanceIsCorrect() {
+        $this->assertNotNull($this->app);
+        $result = new LoginDal($this->app, NULL);
+        $this->assertInstanceOf('Puzzlout\WebIde\DalModules\LoginDal', $result);
+    }
+
+    //Write the next tests below...
 }
