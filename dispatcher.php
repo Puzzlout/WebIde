6bba9b9b28b0069c7a23bfba18fe021fd1dbf007
WebDevJL <webdev.jl@gmail.com>
2016-05-11 06:29:37 +0200

    composer update

--- dispatcher.php
+++ dispatcher.php
@@ -13,22 +13,6 @@ require_once 'errorHandler.php';
  */
 session_start();
 
-use Puzzlout\Framework\Utility\TimeLogger;
-use Puzzlout\Framework\Core\Config;
-use Puzzlout\Framework\Enums\AppSettingKeys;
+use Puzzlout\FrameworkMvc\System\Web\ServerContext;
 
-$errorLogger = new \Puzzlout\Framework\Core\ErrorManager();
-try {
-    $request = new Puzzlout\Framework\Core\Request();
-    $app = new Puzzlout\WebIde\WebIdeApplication($errorLogger);
-    $logGuid = TimeLogger::StartLogInfo($app, "PageLoad => " . Config::Init($app)->Get(AppSettingKeys::ApplicationBaseUrl) . $app->httpRequest()->requestURI());
-    $output = $app->run();
-    TimeLogger::EndLog($app, $logGuid);
-    if ($app->httpRequest()->IsPost()) {
-        echo $output;
-    } else {
-        exit($output);
-    }
-} catch (\Exception $exc) {
-    $errorLogger->LogError($exc);
-}
+$inputs = [];
